507f684a90bbf96152c83105a049c41b3d6e523f
Samuel Hastings <shastings517@gmail.com>
2015-09-09 14:16:56 -0700

    added readfile

--- newLineCount.js
+++ newLineCount.js
@@ -1,8 +1,11 @@
 var fs = require("fs");
 
 
-var str = fs.readFileSync(process.argv[2], "utf-8");
+// var str = fs.readFileSync(process.argv[2], "utf-8");
+fs.readFile(process.argv[2], "utf-8", function(err, data){
 
+  console.log(data.match(/\n/g).length);
+
+});
 // var counter = 0;
-
-console.log(str.match(/\n/g).length);
+// console.log(str.match(/\n/g).length);
